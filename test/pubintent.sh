4161b533534a01f1d029d9875120fe60d409dee1
andreasdominik <andreas.dominik@mni.thm.de>
2019-05-21 09:24:22 +0200

    prepare for testing

---
+++ test/pubintent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -xv
+#
+# publish an example intent
+
+JSON="$1"
+INTENT="$(cat $JSON | grep -o 'andreasdominik:[a-zA-Z0-9]*')"
+TOPIC="hermes/intent/$INTENT"
+
+mosquitto_pub -t "$TOPIC" -f $JSON
