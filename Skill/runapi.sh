5717001ec6172cc763f167e4b676c6bbd910bfc4
andreasdominik <andreas.dominik@mni.thm.de>
2019-05-21 17:32:36 +0200

    next test for write-permissions

--- Skill/runapi.sh
+++ Skill/runapi.sh
@@ -3,23 +3,29 @@
 # Script to test the low-level api (pythonscript)
 # and write permissions in th file system.
 #
-PY=$1
-PA=$2
-TOK=$3
-PIC=$4
-MOD=$5
+# PY=$1
+# PA=$2
+# TOK=$3
+# PIC=$4
+# MOD=$5
 
-cd /tmp
 echo "................"
 echo "................"
+cd /var/lib/snips/skills/ADoSnipsGCalendar/
 pwd
-whoami
 ls -l > ll.txt
-echo "................"
-echo "................"
 cat ll.txt
 env
 echo "................"
 echo "................"
 
-python2 $1 . $3 $4 $5
+echo "................"
+echo "................"
+cd /var/lib/snips/skills/
+pwd
+ls -l > ll.txt
+cat ll.txt
+env
+echo "................"
+echo "................"
+# python2 $1 . $3 $4 $5
